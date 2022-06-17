9811b4f6c80fd6f358b046b004c4044f42afa06c
kijyhj <kijyhj@empal.com>
2022-06-17 13:44:53 +0900

    12121

--- test2022/src/main/java/result/EpCommonCode.java
+++ test2022/src/main/java/result/EpCommonCode.java
@@ -165,7 +165,7 @@ public class EpCommonCode {
      * @mbg.generated Wed Jun 15 17:26:11 KST 2022
      */
     public String getDelYn() {
-        return delYn+"1111";
+        return delYn+"2222";
     }
 
     /**
